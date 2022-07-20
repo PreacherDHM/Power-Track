198fc8f39e9fb3b006a7334c1ab2985c139387e7
PreacherDHM <jesseh2003@outlook.com>
2022-07-20 15:50:03 -0700

    Added the the file formating

---
+++ CMakeFiles/Power-Track.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Power-Track.dir/src/main.cpp.o"
+  "CMakeFiles/Power-Track.dir/src/main.cpp.o.d"
+  "Power-Track"
+  "Power-Track.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Power-Track.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
